d7beb444f6dc4db86d888b0ac812bea80d8296c3
330k <kei.misawa@gmail.com>
2015-03-01 19:31:18 +0900

    update website

--- htmlcompress/htmlcompress.sh
+++ htmlcompress/htmlcompress.sh
@@ -4,3 +4,11 @@ options="--compress-js --compress-css --preserve-comments --remove-intertag-spac
 java -jar htmlcompressor-1.5.3.jar $options -o ../create_gpx_route.html ../create_gpx_route_org.html 
 java -jar htmlcompressor-1.5.3.jar $options -o ../create_gpx_route_mobile.html ../create_gpx_route_mobile_org.html 
 
+echo '<?xml version="1.0" encoding="UTF-8" ?>
+<Module>
+  <ModulePrefs title="Create GPX Route with Google Maps API v3" height="600" scaling="false" />
+  <Content type="html"><![CDATA[' > ../create_gpx_route_gadget.xml
+cat ../create_gpx_route.html >> ../create_gpx_route_gadget.xml
+echo ']]>
+</Content>
+</Module>' >> ../create_gpx_route_gadget.xml
