dec14a6beeeec168596b3b318432d59fbaa816e5
330k <kei.misawa@gmail.com>
2016-08-28 20:34:45 +0900

    modify scripts

--- htmlcompress/htmlcompress.sh
+++ htmlcompress/htmlcompress.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-find ../ -iname "*.png" | xargs optipng -preserve -o7
+# Optimize PNG
+#find ../ -iname "*.png" | xargs optipng -preserve -o7
 
+# Optimize HTML, CSS, JavaScript
 options="--compress-js --compress-css --preserve-comments --remove-intertag-spaces --remove-quotes --remove-style-attr --remove-link-attr --remove-script-attr --remove-form-attr --remove-surrounding-spaces all"
 java -jar htmlcompressor-1.5.3.jar $options -o ../create_gpx_route.html ../create_gpx_route_org.html 
 java -jar htmlcompressor-1.5.3.jar $options -o ../create_gpx_route_mobile.html ../create_gpx_route_mobile_org.html 
 
+# Create XML File for Google Sites Gadget
 echo '<?xml version="1.0" encoding="UTF-8" ?>
 <Module>
   <ModulePrefs title="Create GPX Route with Google Maps API v3" height="600" scaling="false" />
